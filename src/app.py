b636fb4ece8cb30ed08adf0e91e292f116748d00
Anoop P <computerboyhcu@gmail.com>
2024-03-19 16:35:24 +0530

    Add app core code

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    user_agent = request.headers.get("User-Agent")
+    message = "<h1>Welcome to 2022</h1>"
+    return message + "<br>" + user_agent
+
+if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8090)))
+    app.run(debug=True, host='0.0.0.0', port=8090)
